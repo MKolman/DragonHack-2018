52bef725f1c105990b0490f5e1d94fa7f359bbd0
Maks Kolman <kolman.maks@gmail.com>
2018-05-20 00:00:32 +0200

    Fix view name

--- server/alexa_api/urls.py
+++ server/alexa_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('command/<command>', views.parse_command, name='alexa:command'),
+    path('command/<command>', views.parse_command, name='command'),
 ]

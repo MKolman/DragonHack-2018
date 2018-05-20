f21dc954013f7875d08041888bfde14d3b46889e
Maks Kolman <kolman.maks@gmail.com>
2018-05-20 05:12:50 +0200

    Better VR support

--- server/frontend/static/scripts.js
+++ server/frontend/static/scripts.js
@@ -106,7 +106,7 @@ var App = {
       App.rotZ = data.z;
       App.setRotate();
 
-      window.setTimeout(App.fetchOrientation, 100);
+      window.setTimeout(App.fetchOrientation, 10);
     });
   },
 
